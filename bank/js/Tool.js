18ea8845f2d5baf342ba76da7975d340fd6cc266
Chun-Hung Huang <30680380+cubshuang@users.noreply.github.com>
2026-09-21 16:18:06 +0800

    Add files via upload

---
+++ bank/js/Tool.js
@@ -0,0 +1,27 @@
+function request(paras){ 
+    var url = location.href; 
+    var paraString = url.substring(url.indexOf("?")+1,url.length).split("&"); 
+    var paraObj = {} 
+    for (i=0; j=paraString[i]; i++){ 
+        paraObj[j.substring(0,j.indexOf("=")).toLowerCase()] = j.substring(j.indexOf("=")+1,j.length); 
+    } 
+    var returnValue = paraObj[paras.toLowerCase()]; 
+    if(typeof(returnValue)=="undefined"){ 
+        return ""; 
+    }else{ 
+        return returnValue; 
+    } 
+}
+var fName=["113-1","113-2"];//題目卷設定
+let y = request("y"); y = (y!="") ? y:"113";
+let p = request("p"); p = (p!="") ? p:"1";
+let head = document.getElementsByTagName('head')[0]; 
+let script1 = document.createElement('script'); 
+let script2 = document.createElement('script'); 
+let path = 'js/';
+script1.type = script2.type= 'text/javascript'; 
+script1.src = path + 'Exam-'+ y +'-'+ p +'.js'; 
+script2.src = path + 'QA.js'; 
+//head.appendChild(script1).appendChild(script2);
+head.appendChild(script1);
+window.setTimeout( function() { head.appendChild(script2);} , 2000);
